8f6e1d7884d0a65d64bd1e36b46dee7214710d81
joyli <joyli@192.168.254.18>
2019-10-05 01:26:33 +0800

    mergeAddMember

--- OsasWebProject/src/org/pup/system/osas/ui/action/AddMemberAction.java
+++ OsasWebProject/src/org/pup/system/osas/ui/action/AddMemberAction.java
@@ -106,14 +106,6 @@ public class AddMemberAction extends AbstractAction {
 					member.setOrganizationList(new ArrayList<Organization>());
 					for (Integer organizationId : organizationIdList) {
 						Organization organization = new Organization(organizationId);
-						if("A".equals(organization.getOrganizationType().getOrganizationTypeCode())) {
-							acadCtr++;
-						}
-						
-						if(acadCtr > 1) {
-							throw new BusinessException("You can't select more than 1 Academic Organization.");
-						}
-						
 						member.getOrganizationList().add(organization);
 					}
 					
